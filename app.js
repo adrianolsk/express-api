dac05dcac4aa6fd2bcd4503f04ae8490e370ddbf
adrianolsk <adriano.skroch@gmail.com>
2018-05-02 12:24:25 -0300

    fix ignore

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-//ok
+//ok 7
 
 app.get("/", function(req, res) {
     res.send(JSON.stringify(req.query, null, 2));
